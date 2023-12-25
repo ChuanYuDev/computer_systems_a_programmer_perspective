4c4ece1392af5a30c69867a792ce8ed0a8e9db11
nealmatrix <nealmatrixgg@gmail.com>
2023-12-25 10:02:05 +0000

    modified 11.3 dd2hex

--- problem/11.2/hex2dd.c
+++ problem/11.2/hex2dd.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         fprintf(stderr, "usage: %s <hex number>\n", argv[0]);
-        exit(0);
+        exit(1);
     }
 
     sscanf(argv[1], "%x", &addr);
