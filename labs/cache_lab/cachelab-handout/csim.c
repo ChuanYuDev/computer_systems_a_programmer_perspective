4e5e8bee58250e75dcddecc15bca848eb3d2d924
nealmatrix <nealmatrixgg@gmail.com>
2025-03-05 11:50:03 +0000

    add simulation

--- labs/cache_lab/cachelab-handout/csim.c
+++ labs/cache_lab/cachelab-handout/csim.c
@@ -4,12 +4,14 @@
 #include <getopt.h>
 #include "cachelab.h"
 
+static int num_set_bit, num_cache_line, num_block_bit, verbose;
+static int hit_count = 0, miss_count = 0, eviction_count = 0;
 
 struct cache_line
 {
     struct cache_line *ptr_prev;
     struct cache_line *ptr_next;
-    long tag_valid_bit;
+    unsigned long tag_valid_bit;
 };
 
 struct cache_line **initialize_cache(int num_set, int num_cahce_line)
@@ -22,6 +24,7 @@ struct cache_line **initialize_cache(int num_set, int num_cahce_line)
         cache_ptr[i] = (struct cache_line *)malloc(sizeof(struct cache_line) * num_cahce_line);
         cache_line_ptr = cache_ptr[i];
 
+        /* initialize ptr_prev, ptr_next */
         cache_line_ptr->ptr_prev = NULL;
 
         for (int j = 1; j < num_cahce_line - 1; j++)
@@ -36,9 +39,99 @@ struct cache_line **initialize_cache(int num_set, int num_cahce_line)
         {
             cache_line_ptr[num_cahce_line - 1].ptr_prev = cache_line_ptr + (num_cahce_line - 2);
         }
+
+        /* initialize tag bit, valid bit */
+
+        for (int j = 0; j < num_cache_line; j++)
+        {
+            cache_line_ptr[j].tag_valid_bit = 0;
+        }
     }
 
     return cache_ptr;
+}
+
+/* Move the node first, make it the most recently used */
+void move_first(struct cache_line **set_ptr, struct cache_line *line_ptr)
+{
+    if (line_ptr->ptr_prev == NULL)
+        return;
+    
+    line_ptr->ptr_prev->ptr_next = line_ptr->ptr_next;
+
+    if (line_ptr->ptr_next != NULL)
+        line_ptr->ptr_next->ptr_prev = line_ptr->ptr_prev;
+    
+    line_ptr->ptr_prev = NULL;
+    line_ptr->ptr_next = *set_ptr;
+    *set_ptr = line_ptr;
+
+    return;
+}
+
+void simulate(struct cache_line **cache_ptr, unsigned long address)
+{
+    int set_index = (address >> num_block_bit) & (~((-1) << num_set_bit));
+    unsigned tag_bit = address >> (num_set_bit + num_block_bit);
+    // printf("%d\n", set_index);
+
+    int valid_bit = 0;
+
+    struct cache_line *line_ptr = cache_ptr[set_index];
+    struct cache_line *line_lru_ptr = NULL;
+
+    while(line_ptr != NULL)
+    {
+        valid_bit = line_ptr->tag_valid_bit & 1;
+
+        if (!valid_bit)
+        {
+            miss_count++;
+
+            if (verbose)
+                printf(" miss");
+
+            line_ptr->tag_valid_bit = (tag_bit << 1) + 1;
+            break;
+        }
+        else
+        {
+            if (tag_bit == (line_ptr->tag_valid_bit >> 1))
+            {
+                hit_count++;
+
+                if (verbose)
+                    printf(" hit");
+
+                break;
+            }
+        }
+
+        if (line_ptr->ptr_next == NULL)
+        {
+            line_lru_ptr = line_ptr;
+        }
+        
+        line_ptr = line_ptr->ptr_next;
+    }
+
+    if (line_ptr == NULL)
+    {
+        miss_count++;
+        eviction_count++;
+
+        if (verbose)
+            printf(" miss eviction");
+
+        line_lru_ptr->tag_valid_bit = (tag_bit << 1) + 1;
+
+        move_first(cache_ptr + set_index, line_lru_ptr);
+
+        return;
+    }
+
+    move_first(cache_ptr + set_index, line_ptr);
+    return;
 }
 
 int main(int argc, char *argv[])
@@ -82,15 +175,15 @@ int main(int argc, char *argv[])
 
     // exit(EXIT_SUCCESS);
 
-    int num_set_bit = 2, num_cache_line = 2;
+    num_set_bit = 4, num_cache_line = 2, num_block_bit = 4;
+    verbose = 1;
     int num_set = 1 << num_set_bit;
-    // num_block_bit = 4;
 
-    
+    /* Initialize the cache */
+    struct cache_line **cache_ptr = initialize_cache(num_set, num_cache_line);
 
     /* Read the trace file */
-
-    char *filename = "traces/dave.trace";
+    char *filename = "traces/yi.trace";
     char line[256];
 
     FILE *file = fopen(filename, "r");
@@ -98,24 +191,50 @@ int main(int argc, char *argv[])
     if (file != NULL)
     {
         char op;
-        long address;
+        unsigned long address;
         int size;
 
         while (fgets(line, sizeof(line), file))
         {
-            sscanf(line, " %c %ld,%d", &op, &address, &size);
-            printf("%s", line);
+            sscanf(line, " %c %lx,%d", &op, &address, &size);
 
-            printf("%c - %ld - %d\n", op, address, size);
+            if (verbose)
+                printf("%c %lx,%d", op, address, size);
+
+            switch(op)
+            {
+                case 'I':
+                    continue;
+                    break;
+                
+                case 'M': 
+                case 'L':
+                case 'S':
+
+                    /* Simulate */
+                    simulate(cache_ptr, address);
+                    break;
+                
+                default:
+                    fprintf(stderr, "Wrong operation: %c\n", op);
+            }
+
+            if (op == 'M')
+            {
+                hit_count++;
+
+                if (verbose)
+                    printf(" hit");
+            }
+
+            if (verbose)
+                printf("\n");
         }
 
         fclose(file);
     }
 
-
-    /* Initialize the cache */
-    struct cache_line **cache_ptr = initialize_cache(num_set, num_cache_line);
-
+    /* Free cache */
     for (int i = 0; i < num_set; ++i)
     {
         free(cache_ptr[i]);
@@ -123,8 +242,7 @@ int main(int argc, char *argv[])
 
     free(cache_ptr);
 
-    /* Simulate */
-
-    printSummary(0, 0, 0);
+    /* Print summary */
+    printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
